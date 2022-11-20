d359c874aa13d1366aa6a40b434c26a7a638331f
BestLocation <115041500+BestLocation@users.noreply.github.com>
2022-11-19 19:14:47 -0500

    Update Checker.py

--- samples/Checker.py
+++ samples/Checker.py
@@ -2,27 +2,33 @@ import requests
 import json
 import os
 
+date = "2022-11-18"  #Should be one day before current date
 
-date = "2022-11-18" #Should be one day before current date
-
-id = os.environ["CheckerKey"]
+id = "7495e43c0emshb73f62d4a2588dep170bafjsnecdb6397f918"
 url = "https://diagnosis.p.rapidapi.com/api/DDxItems/"
 
-querystring = {"AuthenticationID":"DEMO_AuthenticationID"}
+querystring = {"AuthenticationID": "DEMO_AuthenticationID"}
 
 payload = {
-	"id": "9999",
-	"tests": "[]",
-	"symptoms": "[{\"ID\":1},{\"ID\":18},{\"ID\":103}]"
+  "id": "9999",
+  "tests": "[]",
+  "symptoms": "[{\"ID\":1},{\"ID\":19},{\"ID\":103},{\"ID\":14}]"
 }
 headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": id,
-	"X-RapidAPI-Host": "diagnosis.p.rapidapi.com"
+  "content-type": "application/json",
+  "X-RapidAPI-Key": id,
+  "X-RapidAPI-Host": "diagnosis.p.rapidapi.com"
 }
 
-response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
-x = requests.get(f"https://diagnosisapi.azurewebsites.net/api/DDxItems/9999?AuthenticationID=DEMO_AuthenticationID")
+response = requests.request("POST",
+                            url,
+                            json=payload,
+                            headers=headers,
+                            params=querystring)
+x = requests.get(
+  f"https://diagnosisapi.azurewebsites.net/api/DDxItems/9999?AuthenticationID=DEMO_AuthenticationID"
+)
 y = json.loads(x.text)
-print(y)
+print(y[0]["name"]) #Unrealable Must Regester for full information
+
 
